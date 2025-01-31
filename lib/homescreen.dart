17010c0b039d7a864969537f845b8c92e382f3d7
Chetan <Chetan.sq05@gmail.com>
2025-01-31 18:00:51 +0530

    31/01/25

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/signup.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -13,6 +14,47 @@ class _HomescreenState extends State<Homescreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home screen"),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          PopupMenuButton(
+            onSelected: (value) {
+              if (value == 'YES') {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => Signupscreen()),
+                );
+              }
+            },
+            child: Center(
+              child: Container(
+                height: 50,
+                width: 100,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.black),
+                child: Center(
+                  child: Text(
+                    'Log Out',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                value: 'YES',
+                child: Text('YES'),
+              ),
+              PopupMenuItem(
+                value: 'NO',
+                child: Text('NO'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
